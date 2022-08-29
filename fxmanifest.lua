15fbdb032dfcdbe880e6fbfe22c1e08671e34f0e
andre <darahzmail@gmail.com>
2022-08-29 17:55:32 +0200

    Added serverside and refactured

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,9 @@ author "Darahz"
 description "me"
 version "0.0.1"
 
-client_scripts {
-    'cratedrop_client.lua'    
-}
+client_script 'cratedrop_client.lua'
+
+server_script 'cratedrop_server.lua'
 
 shared_scripts {
     'config.lua'
