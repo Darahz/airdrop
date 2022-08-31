7427c5f94511676c118d7814fb1c4243223bb81a
andre <darahzmail@gmail.com>
2022-08-31 13:37:13 +0200

    Simple cleanup

--- airdrop_server.lua
+++ airdrop_server.lua
@@ -6,5 +6,4 @@ AddEventHandler("airdrop:server:spawnchopper",function()
     local chopperSpawnLocation = airdropConfig.ChopperSpawnLocations[math.random(#airdropConfig.ChopperSpawnLocations)]
     local chopperDropLocation  = airdropConfig.Droplocations[math.random(#airdropConfig.Droplocations)]
     TriggerClientEvent("airdrop:client:spawnchopper", -1,chopperSpawnLocation,chopperDropLocation)
-
 end)
